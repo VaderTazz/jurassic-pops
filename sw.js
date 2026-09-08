af914763269668f025cbe1c2b07aa74704b8abff
VaderTazz <auzzdad@gmail.com>
2026-09-08 13:36:28 +1000

    Add service worker

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE_NAME = 'app-v1';
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { self.clients.claim(); });
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request, {cache: 'no-store'}).catch(() => caches.match(e.request)));
+});
